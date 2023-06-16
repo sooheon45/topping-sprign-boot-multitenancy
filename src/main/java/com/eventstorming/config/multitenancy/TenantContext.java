b1ad51bdbaf30a75f8cef4f602397469a89225f1
sooheon45 <54785805+sooheon45@users.noreply.github.com>
2023-06-16 11:38:13 +0900

    Create TenantContext.java

---
+++ src/main/java/com/eventstorming/config/multitenancy/TenantContext.java
@@ -0,0 +1,16 @@
+path: {{name}}/{{{options.packagePath}}}/config/multitenancy
+---
+package {{options.package}}.config.multitenancy;
+
+public class TenantContext {
+
+    private static final ThreadLocal<String> CURRENT_TENANT = new ThreadLocal<>();
+
+    public static String getCurrentTenant() {
+        return CURRENT_TENANT.get();
+    }
+
+    public static void setCurrentTenant(String tenant) {
+        CURRENT_TENANT.set(tenant);
+    }
+}
